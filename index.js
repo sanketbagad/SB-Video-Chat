11412b9a4349ef3afebb32dc1b361477546a02e2
Sanket Bagad <alencolins@gmail.com>
2021-10-04 12:38:44 +0530

    Starting with backend

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require("express")()
+const server = require("http").createServer(app)
+const cors = require(cors)
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin:"*",
+        methods: ["GET", "POST"]
+    }
+})
+
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send("Server is running")
+})
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callended")
+    })
+
+    socket.on("calluser", ({ userToCall, sighnalData, from, name }) => {
+        io.to(userToCall).emit("calluser", {signal: sighnalData, from, name })
+    })
+
+    socket.on("answercall", (data) => {
+        io.to(data).emit("call accepted", data.signal)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log("Fucking server listening on port")
+})
+
+
+
