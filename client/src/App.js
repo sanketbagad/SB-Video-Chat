5a759398b6feb24f81051ecdd2bba3ef2c71be0f
Sanket Bagad <alencolins@gmail.com>
2021-10-06 01:21:42 +0530

    Working with socket at client side

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,43 @@ import {Typography, AppBar} from '@material-ui/core'
 import VideoPlayer from './Components/VideoPlayer'
 import Options from './Components/Options'
 import Notifications from './Components/Notifications'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 100px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '600px',
+        border: '2px solid black',
+    
+        [theme.breakpoints.down('xs')]: {
+          width: '90%',
+        },
+      },
+      image: {
+        marginLeft: '15px',
+      },
+      wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+      },
+}))
+
 
 const App = () => {
+    const classes = useStyles()
+
     return (
-        <div>
-           <AppBar position="static" color="inherit">
+        <div className={classes.wrapper}>
+           <AppBar className={classes.appBar} position="static" color="inherit">
             <Typography variant="h2" align="center">
-                Video Chat
+                SB__MEET
             </Typography>
            </AppBar>
 
