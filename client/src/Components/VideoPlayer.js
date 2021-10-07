9adaf58f4b9078bcacc3b7d6d955add5e19eec7b
Sanket Bagad <alencolins@gmail.com>
2021-10-08 02:10:17 +0530

    Working on Video Player

--- client/src/Components/VideoPlayer.js
+++ client/src/Components/VideoPlayer.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { Grid, Typography, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    video: {
+        width: '550px',
+        [theme.breakpoints.down('xs')]: {
+          width: '300px',
+        },
+      },
+      gridContainer: {
+        justifyContent: 'center',
+        [theme.breakpoints.down('xs')]: {
+          flexDirection: 'column',
+        },
+      },
+      paper: {
+        padding: '10px',
+        border: '2px solid black',
+        margin: '10px',
+      },
+}))
 
 const VideoPlayer = () => {
+    const { name, myVideo, userVideo, callAccepted, callEnded, stream, call} = useContext(SocketContext)
+    const classes = useStyles()
     return (
-        <div>
-            
-        </div>
+        <Grid container className={classes.gridContainer}>
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>
+                        Name
+                    </Typography>
+                    <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>
+                        Name
+                    </Typography>
+                    <video playsInline  ref={userVideo} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+        </Grid>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
+import {ContextProvider} from "./SocketContext"
 import "./styles.css"
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+<ContextProvider>
+<App />
+</ContextProvider>
+, document.getElementById('root'))
